58f1b10420644df8a50e0ed12c842fd28469589d
vinniaraujjo <vfa43171@uga.edu>
2026-02-03 13:55:59 -0500

    DATA_WRANGLING

---
+++ code/01-15_IntrotoR_partial.R
@@ -0,0 +1,100 @@
+## 2026 CRSS 8030 - Jan 15 Agenda
+
+# Housekeeping:
+## Create a folder named 01_intro at the main folder for this course, and move this script into it
+## Website (bookmark it!): https://leombastos.github.io/bastoslab/teaching/2026-dsa/2026-dsa.html
+## GitHub: material, constantly being updated 
+## YouTube: recordings  
+
+# Setting up RStudio:
+## Tools > Global Options > General > Workspace > uncheck "Restore ...", select "Never" on dropdown menu  
+## Tools > Global Options > R markdown > Show in document outline > Sections and Named Chunks
+
+
+# Learning objectives ----
+# - Become familiarized with using R and RStudio
+# - Learn about R terminology and syntax
+# - Understand different object types
+# - Create a simple data, explore it with numbers and graphics
+# - Learn about RStudio projects, create your own, set up proper sub-directories  
+
+
+# 1) R/Rstudio ----
+
+## Why R? 
+### free, 
+### runs on multiple platforms, 
+### online community and support, 
+### continuous development, 
+### reproducible research!
+
+## Why RStudio?
+### Integrates various components of an analysis
+### Colored syntax
+### Syntax suggestions
+
+## RStudio panels
+### Script  
+### Console
+### Environment
+### Files/Plots/Help
+
+# 2) R terminology ----
+# Object
+
+## Object classes
+### Data frame
+
+### Matrices
+
+### Lists 
+
+## Function
+
+## Argument
+
+## Package
+## Install vs. load a package
+# install.packages("tibble")
+
+## Let's install package tibble, then load it
+
+# 3) Creating a data set, exploring it ----
+
+# Check class, summary, and structure
+
+# Sampling the dataset
+# First row only
+
+# First column only
+
+# Rows 1 to 3 and columns 1 to 3
+
+# Rows 1 and 3 and columns 1 and 3
+
+# 4) ggplot2 philosophy and plots ---- 
+#install.packages("ggplot2")
+
+# Point
+
+# Customizing
+
+# Exporting
+
+# 5) RStudio projects ----
+## Inside of your course main folder, create a sub-folder called 02_datawrangling
+## Create sub-folders data, code, output
+## Create an RStudio project at the level of main folder 02_datawrangling
+## Create an Rmarkdwon file (just to explore, not saving it) 
+
+# 6) Assignment #1 - Play with ggplot
+# Play with scale_color_ , explore the available options, choose one different from the one in class
+# play with theme_ , explore the available options, choose one different from the one in class
+# Export your new version saving it as "A1_firstname_lastname.png"
+# Upload your new plot to eLC under "Assignment #1"
+
+
+
+
+
+
